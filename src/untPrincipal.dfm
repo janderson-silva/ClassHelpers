41a6057726fa6fa0e092fe6646594482e91d716a
Janderson Silva <janderson97.rm@gmail.com>
2025-08-20 23:01:13 -0400

    update

--- src/untPrincipal.dfm
+++ src/untPrincipal.dfm
@@ -1,14 +1,117 @@
 object Form1: TForm1
   Left = 0
   Top = 0
-  Caption = 'Form1'
-  ClientHeight = 441
-  ClientWidth = 624
+  Caption = 'Demonstracao StringHelper - Class Helpers'
+  ClientHeight = 350
+  ClientWidth = 600
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
   Font.Height = -12
   Font.Name = 'Segoe UI'
   Font.Style = []
+  Position = poScreenCenter
   TextHeight = 15
+  object lblTexto: TLabel
+    Left = 16
+    Top = 16
+    Width = 81
+    Height = 15
+    Caption = 'Texto de Teste:'
+  end
+  object lblNumero: TLabel
+    Left = 16
+    Top = 64
+    Width = 93
+    Height = 15
+    Caption = 'Numero de Teste:'
+  end
+  object edtTexto: TEdit
+    Left = 16
+    Top = 37
+    Width = 350
+    Height = 23
+    TabOrder = 0
+    Text = 'Hello World - Teste de String Helper!'
+  end
+  object edtNumero: TEdit
+    Left = 16
+    Top = 85
+    Width = 150
+    Height = 23
+    TabOrder = 1
+    Text = '123.45'
+  end
+  object btnTest: TButton
+    Left = 400
+    Top = 37
+    Width = 75
+    Height = 25
+    Caption = 'Teste'
+    TabOrder = 2
+    OnClick = btnTestClick
+  end
+  object btnReverse: TButton
+    Left = 16
+    Top = 120
+    Width = 100
+    Height = 25
+    Caption = 'Reverse'
+    TabOrder = 3
+    OnClick = btnReverseClick
+  end
+  object btnRemoveSpaces: TButton
+    Left = 122
+    Top = 120
+    Width = 100
+    Height = 25
+    Caption = 'RemoveSpaces'
+    TabOrder = 4
+    OnClick = btnRemoveSpacesClick
+  end
+  object btnCapitalize: TButton
+    Left = 228
+    Top = 120
+    Width = 100
+    Height = 25
+    Caption = 'Capitalize'
+    TabOrder = 5
+    OnClick = btnCapitalizeClick
+  end
+  object btnOnlyNumbers: TButton
+    Left = 334
+    Top = 120
+    Width = 100
+    Height = 25
+    Caption = 'OnlyNumbers'
+    TabOrder = 6
+    OnClick = btnOnlyNumbersClick
+  end
+  object btnWordCount: TButton
+    Left = 16
+    Top = 151
+    Width = 100
+    Height = 25
+    Caption = 'WordCount'
+    TabOrder = 7
+    OnClick = btnWordCountClick
+  end
+  object btnIsEmpty: TButton
+    Left = 122
+    Top = 151
+    Width = 100
+    Height = 25
+    Caption = 'IsEmpty'
+    TabOrder = 8
+    OnClick = btnIsEmptyClick
+  end
+  object btnToInteger: TButton
+    Left = 172
+    Top = 85
+    Width = 75
+    Height = 23
+    Caption = 'ToInteger'
+    TabOrder = 9
+    OnClick = btnToIntegerClick
+  end
 end
--- src/untPrincipal.pas
+++ src/untPrincipal.pas
@@ -3,11 +3,31 @@ unit untPrincipal;
 interface
 
 uses
-  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes,
+  Vcl.Graphics, Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, StringHelper;
 
 type
   TForm1 = class(TForm)
+    edtTexto: TEdit;
+    lblTexto: TLabel;
+    btnTest: TButton;
+    btnReverse: TButton;
+    btnRemoveSpaces: TButton;
+    btnCapitalize: TButton;
+    btnOnlyNumbers: TButton;
+    btnWordCount: TButton;
+    btnIsEmpty: TButton;
+    edtNumero: TEdit;
+    lblNumero: TLabel;
+    btnToInteger: TButton;
+    procedure btnTestClick(Sender: TObject);
+    procedure btnReverseClick(Sender: TObject);
+    procedure btnRemoveSpacesClick(Sender: TObject);
+    procedure btnCapitalizeClick(Sender: TObject);
+    procedure btnOnlyNumbersClick(Sender: TObject);
+    procedure btnWordCountClick(Sender: TObject);
+    procedure btnIsEmptyClick(Sender: TObject);
+    procedure btnToIntegerClick(Sender: TObject);
   private
     { Private declarations }
   public
@@ -20,5 +40,72 @@ var
 implementation
 
 {$R *.dfm}
+
+procedure TForm1.btnTestClick(Sender: TObject);
+var
+  S: String;
+begin
+  S := edtTexto.Text;
+  ShowMessage('Teste Reverse: ' + S.Reverse);
+end;
+
+procedure TForm1.btnReverseClick(Sender: TObject);
+var
+  S: String;
+begin
+  S := edtTexto.Text;
+  ShowMessage('Resultado: ' + S.Reverse);
+end;
+
+procedure TForm1.btnRemoveSpacesClick(Sender: TObject);
+var
+  S: String;
+begin
+  S := edtTexto.Text;
+  ShowMessage('Resultado: ' + S.RemoveSpaces);
+end;
+
+procedure TForm1.btnCapitalizeClick(Sender: TObject);
+var
+  S: String;
+begin
+  S := edtTexto.Text;
+  ShowMessage('Resultado: ' + S.Capitalize);
+end;
+
+procedure TForm1.btnOnlyNumbersClick(Sender: TObject);
+var
+  S: String;
+begin
+  S := edtTexto.Text;
+  ShowMessage('Resultado: ' + S.OnlyNumbers);
+end;
+
+procedure TForm1.btnWordCountClick(Sender: TObject);
+var
+  S: String;
+begin
+  S := edtTexto.Text;
+  ShowMessage('Palavras: ' + IntToStr(S.WordCount));
+end;
+
+procedure TForm1.btnIsEmptyClick(Sender: TObject);
+var
+  S: String;
+begin
+  S := edtTexto.Text;
+  if S.IsEmpty then
+    ShowMessage('Texto está vazio')
+  else
+    ShowMessage('Texto não está vazio');
+end;
+
+procedure TForm1.btnToIntegerClick(Sender: TObject);
+var
+  S: String;
+begin
+  S := edtNumero.Text;
+  ShowMessage('Resultado: ' + IntToStr(S.ToIntegerDef));
+end;
 
 end.
